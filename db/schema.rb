32f47253b3c032b681537659d3222d2d791f641a
Bludidi <bludidi5@gmail.com>
2023-02-09 16:18:33 +0200

    Create application.yml in local env

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_08_095556) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_09_054119) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_08_095556) do
     t.text "Text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["AuthorId"], name: "index_comments_on_AuthorId"
+    t.index ["PostId"], name: "index_comments_on_PostId"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -27,28 +29,30 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_08_095556) do
     t.integer "PostId"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["AuthorId"], name: "index_likes_on_AuthorId"
+    t.index ["PostId"], name: "index_likes_on_PostId"
   end
 
   create_table "posts", force: :cascade do |t|
     t.integer "AuthorId"
-    t.string "Title"
+    t.text "Title"
     t.text "Text"
     t.integer "CommentsCounter"
     t.integer "LikesCounter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["AuthorId"], name: "index_posts_on_AuthorId"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "Name"
-    t.string "Bio"
-    t.string "Image"
+    t.string "Photo"
+    t.text "Bio"
     t.integer "PostsCounter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "comments", "posts", column: "PostId"
   add_foreign_key "comments", "users", column: "AuthorId"
   add_foreign_key "likes", "posts", column: "PostId"
   add_foreign_key "likes", "users", column: "AuthorId"
